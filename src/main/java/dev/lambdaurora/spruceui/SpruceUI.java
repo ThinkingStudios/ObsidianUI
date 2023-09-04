38f0216ade6ddb9aeab46befa9a4f91cb408d989
TexTrue <3140846162@qq.com>
2023-09-04 08:34:58 +0800

    fix

--- src/main/java/dev/lambdaurora/spruceui/SpruceUI.java
+++ src/main/java/dev/lambdaurora/spruceui/SpruceUI.java
@@ -3,13 +3,17 @@ package dev.lambdaurora.spruceui;
 import dev.architectury.utils.EnvExecutor;
 import dev.lambdaurora.spruceui.hud.HudManager;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(SpruceUI.MODID)
 public class SpruceUI {
     public static final String MODID = "spruceui";
 
     public SpruceUI() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         EnvExecutor.runInEnv(Dist.CLIENT, () -> this::onInitializeClient);
     }
 
