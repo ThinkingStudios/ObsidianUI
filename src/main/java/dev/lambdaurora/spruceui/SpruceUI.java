9e59134a7b5123ac195e05aac46cad5b48210020
TexTrue <3140846162@qq.com>
2023-09-04 08:36:30 +0800

    fix

--- src/main/java/dev/lambdaurora/spruceui/SpruceUI.java
+++ src/main/java/dev/lambdaurora/spruceui/SpruceUI.java
@@ -1,13 +1,11 @@
 package dev.lambdaurora.spruceui;
 
-import dev.architectury.platform.forge.EventBuses;
 import dev.architectury.utils.EnvExecutor;
 import dev.lambdaurora.spruceui.hud.HudManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkConstants;
 
 @Mod(SpruceUI.MODID)
@@ -16,7 +14,6 @@ public class SpruceUI {
 
     public SpruceUI() {
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-        EventBuses.registerModEventBus(MODID, FMLJavaModLoadingContext.get().getModEventBus());
         EnvExecutor.runInEnv(Dist.CLIENT, () -> this::onInitializeClient);
     }
 
