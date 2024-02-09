bacc32c2f579f74a4ef33238a6fedf0a4e3fa5f6
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-02-09 16:15:17 +0800

    clean

--- common/src/main/java/org/thinkingstudio/obsidianui/ObsidianUI.java
+++ common/src/main/java/org/thinkingstudio/obsidianui/ObsidianUI.java
@@ -10,12 +10,17 @@
 
 package org.thinkingstudio.obsidianui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.thinkingstudio.obsidianui.hud.HudManager;
 
 public class ObsidianUI {
     public static final String MODID = "obsidianui";
+    public static final Logger LOGGER = LoggerFactory.getLogger("ObsidianUI");
 
     public static void clientInit() {
         new HudManager().initialize();
+
+        LOGGER.info("ObsidianUI is loaded!");
     }
 }
